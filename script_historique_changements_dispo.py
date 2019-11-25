183c6fdd27df7c22cc2ebf0ab8a52a1220beed66
Juliette Desormonts <juliette.desormonts@gmail.com>
2019-11-25 19:57:56 +0100

    liaisons commentaires

--- script_historique_changements_dispo.py
+++ script_historique_changements_dispo.py
@@ -28,6 +28,7 @@ for listing_id in listing_ids:  # pour chaque annonce
     i = 0
     j = 1
     interpretation[listing_id] = {}
+    interpretation[listing_id]["Details"] = {}
     temp_month1 = {}  # Pour stocker le mois 2 et ne pas avoir à le recharger via SQL en mois 1 lors de la prochaine comparaison
 
     # Liste des véritables périodes indisponibles
@@ -156,62 +157,169 @@ for listing_id in listing_ids:  # pour chaque annonce
                 reservations += 1
             if dispo == 'f' and dispo2 == 't':
                 annulations += 1
-        interpretation[listing_id][str(paire)]["Reservations"] = {
-            "nombre total de jours": reservations,
-            str(paire[0]): month1_reservations_liste,
-            str(paire[1]): month2_reservations_liste,
-        }
+        # interpretation[listing_id][str(paire)]["Reservations"] = {
+        #     "nombre total de jours": reservations,
+        #     str(paire[0]): month1_reservations_liste,
+        #     str(paire[1]): month2_reservations_liste,
+        # }
+        interpretation[listing_id][str(paire)]["Reservations"] = reservations
         interpretation[listing_id][str(paire)]["Annulations"] = annulations
 
-        interpretation[listing_id][str(paire[0])]=month1_reservations_liste
-        interpretation[listing_id][str(paire[1])]=month2_reservations_liste
+        interpretation[listing_id]["Details"][str(
+            paire[0])] = month1_reservations_liste
+        interpretation[listing_id]["Details"][str(
+            paire[1])] = month2_reservations_liste
 
-        # print(paire)
-        # print(month1_dispos)
-        # print(month1_reservations_liste)
-        # print(month2_dispos)
-        # print(month2_reservations_liste)        
         i = j
         j += 1
-    
+
     compteur_general = 1
     for scraping in tab_date:
-        month_reservations = interpretation[listing_id][str(scraping)]
+        month_reservations = interpretation[listing_id]["Details"][str(
+            scraping)]
         month_reservations_dates = []
         for periode in month_reservations:
-            periode_date = (datetime.datetime.strptime(periode[0], "%Y-%m-%d").date(), datetime.datetime.strptime(periode[1], "%Y-%m-%d").date())
+            periode_date = (datetime.datetime.strptime(
+                periode[0], "%Y-%m-%d").date(), datetime.datetime.strptime(periode[1], "%Y-%m-%d").date())
             month_reservations_dates.append(periode_date)
-        month_reservations_dates = sorted(month_reservations_dates,key=lambda x:x[0].day)
+        month_reservations_dates = sorted(
+            month_reservations_dates, key=lambda x: x[0].day)
 
         temp = [0]*num_days
-        compteur=0
+        compteur = 0
         for periode in month_reservations_dates:
-            compteur+=1
-            for i in range(periode[0].day,periode[1].day+1):
-                temp[i-1]=compteur
-        if str(listing_id)=="11899722":
-            print("Temp")
-            print(temp)
-            print()
+            compteur += 1
+            for i in range(periode[0].day, periode[1].day+1):
+                temp[i-1] = compteur
 
         for date in range(num_days):
-            # if str(listing_id)=="4723995":
-            #     print("compteur_general "+str(compteur_general))
-            #     print()
-            #Réservation
-            if synthese_periodes_indisponibles[date]==0 and temp[date]!=0:
-                synthese_periodes_indisponibles[date]=compteur_general
-            #Annulation
-            if synthese_periodes_indisponibles[date]!=0 and temp[date]==0:
-                synthese_periodes_indisponibles[date]=0
-            if date!=num_days-1:
-                if temp[date]!=0 and temp[date+1]!=temp[date]:
-                    compteur_general+=1
-        if str(listing_id)=="11899722":
-            print("Synthese")
-            print(synthese_periodes_indisponibles)
-            print()
-            print('-----------')
+            # Réservation
+            if synthese_periodes_indisponibles[date] == 0 and temp[date] != 0:
+                synthese_periodes_indisponibles[date] = compteur_general
+            # Annulation
+            if synthese_periodes_indisponibles[date] != 0 and temp[date] == 0:
+                synthese_periodes_indisponibles[date] = 0
+            if date != num_days-1:
+                if temp[date] != 0 and temp[date+1] != temp[date]:
+                    compteur_general += 1
+
+    compteur_day = 0
+    liste_periodes = []
+    # Initialisation des compteurs (car on ne peut pas comparer à un jour précédent)
+    if synthese_periodes_indisponibles[0] == 0:
+        compteur_periodes = 0
+        compteur_day = 0
+    else:
+        date_debut = datetime.date(year, month, 1)
+        compteur_periodes = 1
+        compteur_day = 1
+    for i in range(1, num_days):
+        if synthese_periodes_indisponibles[i] != synthese_periodes_indisponibles[i-1]:
+            if synthese_periodes_indisponibles[i] != 0:
+                compteur_periodes += 1
+            if synthese_periodes_indisponibles[i] != 0 and synthese_periodes_indisponibles[i-1] != 0:
+                date_fin = datetime.date(year, month, i)
+                liste_periodes.append((date_debut, date_fin))
+                date_debut = datetime.date(year, month, i+1)
+            if synthese_periodes_indisponibles[i] == 0 and synthese_periodes_indisponibles[i-1] != 0:
+                date_fin = datetime.date(year, month, i)
+                liste_periodes.append((date_debut, date_fin))
+            if synthese_periodes_indisponibles[i-1] == 0 and synthese_periodes_indisponibles[i] != 0:
+                date_debut = datetime.date(year, month, i+1)
+        if synthese_periodes_indisponibles[i] != 0:
+            compteur_day += 1
+    if synthese_periodes_indisponibles[-1] != 0:
+        date_fin = datetime.date(year, month, num_days)
+        liste_periodes.append((date_debut, date_fin))
+
+    liste_periodes_string = []
+    for periode in liste_periodes:
+        liste_periodes_string.append((periode[0].strftime(
+            "%Y-%m-%d"), periode[1].strftime("%Y-%m-%d")))
+    if liste_periodes != [(datetime.date(year, month, 1), datetime.date(year, month, num_days))]:
+        interpretation[listing_id]["Synthese"] = {
+            "Jours indisponibles": compteur_day,
+            "Nombre de reservations distinctes": compteur_periodes,
+            "Liste": liste_periodes_string
+        }
+    # Si le propriétaire a fermé tout le mois
+    else:
+        if compteur_day == num_days:
+            interpretation[listing_id]["Synthese"] = "Le proprietaire a ferme tout le mois."
+        if compteur_day == 0:
+            interpretation[listing_id]["Synthese"] = "Le proprietaire n'a pas eu de reservations."
+
+    # Liaison avec les commentaires de l'annonce
+    liaisons = {}
+    if liste_periodes != [(datetime.date(year, month, 1), datetime.date(year, month, num_days))] and liste_periodes != []:
+        sql = "select * from listing_and_reviews where listing_id='" + \
+            str(listing_id)+"' and scraping_date = '" + \
+                str(tab_date[0])+"' and id not null order by date"
+        c.execute(sql)
+        commentaires = c.fetchall()
+        #ETAPE 1 : commentaires dans les 15 jours après la réservation
+        for periode in liste_periodes:
+            liaisons[str(periode)]={}
+            for commentaire in commentaires:
+                date_du_commentaire = datetime.datetime.strptime(
+                commentaire[2], "%Y-%m-%d").date()
+                if periode[1] < date_du_commentaire and periode[1] > date_du_commentaire-datetime.timedelta(days=14):                    
+                    liaisons[str(periode)][commentaire[4]]={
+                        "date":commentaire[2],
+                        "reviewer_id":commentaire[5],
+                        "reviewer_name":commentaire[6],
+                        "contenu":commentaire[7]
+                    }
+        #ETAPE 2 : compter le nombre de réservations qui se sont terminées 15 jours max avant chaque commentaire
+        #Pour déterminer le pourcentage de chance qu'un commentaire corresponde à une résa.
+        for commentaire in commentaires:
+            nb_resa = 0
+            date_du_commentaire = datetime.datetime.strptime(
+                commentaire[2], "%Y-%m-%d").date()
+            for periode in liste_periodes:
+                if periode[1] < date_du_commentaire and periode[1] > date_du_commentaire-datetime.timedelta(days=14):
+                    nb_resa += 1                    
+            for periode in liaisons:
+                if commentaire[4] in liaisons[str(periode)]:
+                    pourcentage = (1/nb_resa)*100
+                    liaisons[str(periode)][commentaire[4]]["pourcentage"]=(1/nb_resa)*100
+        #ETAPE 3 : retirer les commentaires de moins de 100% pour une réservation qui a déjà un commentaire à 100%
+        continuer = True
+        while continuer:
+            continuer = False        
+            for periode in liste_periodes:            
+                found = False
+                id_a_modif = []
+                #Pour chaque période, on cherche si un des commentaires est à 100%
+                for commentaire in liaisons[str(periode)]:
+                    if liaisons[str(periode)][commentaire]["pourcentage"]==100:
+                        id_Cent = commentaire
+                        found = True
+                    else:
+                        id_a_modif.append(commentaire)
+                #Si c'est le cas
+                if found and id_a_modif!=[]:
+                    print(periode)
+                    print(id_a_modif)
+                    temp = {}
+                    #On ne laisse que le commentaire à 100% pour cette période
+                    for commentaire in liaisons[str(periode)]:
+                        if commentaire == id_Cent:
+                            temp[commentaire] = liaisons[str(periode)][commentaire]
+                    liaisons[str(periode)]=temp
+                    #On met à jour les pourcentages des commentaires liés à d'autres périodes
+                    for id in id_a_modif:
+                        for periode in liste_periodes:
+                            if id in liaisons[str(periode)]:
+                                ancien_nb_resa = int(100/liaisons[str(periode)][id]["pourcentage"])
+                                nouveau_pourcentage = (1/(ancien_nb_resa-1))*100
+                                liaisons[str(periode)][id]["pourcentage"]= nouveau_pourcentage
+                                if not continuer and nouveau_pourcentage == 100:
+                                    continuer = True
+    
+    with open('liaisons_'+str(listing_id)+'.json', 'w') as outfile:
+        json.dump(liaisons, outfile)
+
 with open('results.json', 'w') as outfile:
     json.dump(interpretation, outfile)
 print("Fait.")
